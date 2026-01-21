7b5c29ebc340a8d00a46387e9e5456e65b53fb59
oidex69 <shresthaprince222@gmail.com>
2026-01-21 13:46:19 +0545

    first commit

---
+++ src/main/java/zpt/dsaVis/DsaVisApplication.java
@@ -0,0 +1,13 @@
+package zpt.dsaVis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaVisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaVisApplication.class, args);
+	}
+
+}
